8ed6831f4e7fbff2e646d871a9b655535897b4d0
sorenrc-DEV <carley.sorensen@bellevuecollege.edu>
2026-02-05 07:01:04 -0800

    Delete HTML_Form_with _CSS directory

--- HTML_Form_with _CSS/script.js
+++
@@ -1,9 +0,0 @@
-const params = new URLSearchParams(window.location.search);
-
-let output = "";
-
-for (let pair of params.entries()) {
-    output += "<p><strong>" + pair[0] + ":</strong> " + pair[1] + "</p>";
-}
-
-document.getElementById("results").innerHTML = output;
